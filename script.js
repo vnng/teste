b332da0d62f8a9d320822075a71ec1ba61ca5a7d
Pedro Coimbra <pedro.basilio.coimbra@gmail.com>
2022-03-31 10:42:52 -0300

    css finalizado e mudanças no js e html

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ var div = document.getElementById('div1');
 var timetoclick;
 var begintime;
 var endtime;
+var alternate = true
 
 function startGame(){
-    div.style.background = 'red';
     var now = new Date().getTime();
-    while(new Date().getTime() < now + 2000 + (Math.floor(Math.random()*3000))){}
+    while(new Date().getTime() < now + 1000 + (Math.floor(Math.random()*4000))){}
     goingGame();
 }
 
 function stopGame(){
     endtime = new Date().getTime();
-    div.style.background ="yellow";
+    div.style.background ="red";
     timetoclick = (endtime - begintime)/1000;
     var p2 = document.getElementById('p2');
     p2.innerHTML = "Tempo de reação = " + timetoclick;
@@ -21,4 +21,16 @@ function stopGame(){
 function goingGame(){
     begintime = new Date().getTime();
     div.style.background = "green";
+}
+
+function alternateCall(){
+    if(alternate){
+        alternate = false;
+        startGame();
+    }
+    else{
+        alternate = true
+        stopGame();
+    }
+
 }
